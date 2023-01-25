7f9d806621d567195831ca5e639ccf3ad0e7f5e6
zapxcero <47491562+zapxcero@users.noreply.github.com>
2023-01-25 14:06:00 +0000

    Added current_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ require 'rest-client'
 require 'pry'
 
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= session[:user_id]
+  end
 end
