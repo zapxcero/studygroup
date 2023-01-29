099f73c4197b053085c75dc4733a0474b11097f4
AdrianRodrigu27 <>
2023-01-29 15:06:08 +0000

    Change SUPABASE_URL to supabase_url helper method and displays creator and attendees names instead of uuid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,13 @@ require 'pry'
 
 class ApplicationController < ActionController::Base
   helper_method :current_user
-
-  SUPABASE_URL = 'https://lwznbznauavlibpsepta.supabase.co'.freeze
+  helper_method :supabase_url
 
   def current_user
     @current_user ||= session[:user_id]
+  end
+
+  def supabase_url
+    'https://lwznbznauavlibpsepta.supabase.co'.freeze
   end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,7 @@ class EventsController < ApplicationController
       'Authorization': "Bearer #{ENV['supabase_api_key']}"
     }
 
-    response = RestClient.get("#{SUPABASE_URL}/rest/v1/events?select=id,name,location,date,creator_id", headers)
+    response = RestClient.get("#{supabase_url}/rest/v1/events?select=id,name,location,date,creator_id", headers)
     gz = Zlib::GzipReader.new(StringIO.new(response.body))
     @events = JSON.parse(gz.read)
   end
@@ -24,7 +24,7 @@ class EventsController < ApplicationController
 
     data = { name: params[:event][:name], location: params[:event][:location], date: params[:event][:date], creator_id: current_user }
 
-    response = RestClient.post "#{SUPABASE_URL}/rest/v1/events", data.to_json, headers
+    response = RestClient.post "#{supabase_url}/rest/v1/events", data.to_json, headers
 
     redirect_to root_path, notice: 'Study Group created successfully!' if response.code >= 200 && response.code < 300
   end
@@ -35,7 +35,7 @@ class EventsController < ApplicationController
       Authorization: "Bearer #{ENV['supabase_api_key']}",
       Range: '0-9'
     }
-    url = "#{SUPABASE_URL}/rest/v1/events?id=eq.1&select=id,name,location,date"
+    url = "#{supabase_url}/rest/v1/events?id=eq.1&select=id,name,location,date"
 
     response = RestClient.get(url, headers)
     @data = JSON.parse(response.body).first
@@ -48,7 +48,7 @@ class EventsController < ApplicationController
       Content_Type: 'application/json',
       Prefer: 'return=minimal'
     }
-    url = "#{SUPABASE_URL}/rest/v1/events?id=eq.#{params[:id]}"
+    url = "#{supabase_url}/rest/v1/events?id=eq.#{params[:id]}"
 
     payload = { name: params[:event][:name], location: params[:event][:location], date: params[:event][:date] }.to_json
     response = RestClient.patch(url, payload, headers)
@@ -60,7 +60,7 @@ class EventsController < ApplicationController
       apikey: ENV['supabase_api_key'],
       Authorization: "Bearer #{ENV['supabase_api_key']}"
     }
-    url = "#{SUPABASE_URL}/rest/v1/events?id=eq.#{params[:id]}"
+    url = "#{supabase_url}/rest/v1/events?id=eq.#{params[:id]}"
 
     RestClient.delete(url, headers)
     redirect_to root_path, notice: 'Study Group deleted succesfully!'
