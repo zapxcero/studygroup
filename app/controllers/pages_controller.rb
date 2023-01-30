3aff8de1e3bc41276aa5b3a552cf8fc4a7661642
AdrianRodrigu27 <>
2023-01-30 06:24:45 +0000

    DRY out login/signup actions and uses reset session in logout action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,17 +4,11 @@ class PagesController < ApplicationController
   def login; end
 
   def login_create
-    headers = {
-      'apikey': ENV['supabase_api_key'],
-      'Content-Type': 'application/json'
-    }
-
     payload = {
       email: params[:email],
       password: params[:password]
     }
-    response = RestClient.post("#{supabase_url}/auth/v1/token?grant_type=password", payload.to_json, headers)
-    data = JSON.parse(response.body)
+    data = api_call('/auth/v1/token?grant_type=password', payload)
     session[:user_id] = data['user']['id']
     session[:data] = data
     redirect_to root_path, notice: 'Logged in successfully!'
@@ -23,11 +17,6 @@ class PagesController < ApplicationController
   def signup; end
 
   def signup_create
-    headers = {
-      'apikey': ENV['supabase_api_key'],
-      'Content-Type': 'application/json'
-    }
-
     payload = {
       email: params[:email],
       password: params[:password],
@@ -35,15 +24,27 @@ class PagesController < ApplicationController
         full_name: params[:full_name]
       }
     }
-    response = RestClient.post("#{supabase_url}/auth/v1/signup", payload.to_json, headers)
-    data = JSON.parse(response.body)
+    data = api_call('/auth/v1/signup', payload)
     session[:user_id] = data['id']
     session[:data] = data
     redirect_to root_path, notice: 'Signed up successfully!' if response.code >= 200 && response.code < 300
   end
 
   def logout
-    session.clear
-    redirect_to root_path, noticed: 'Logged out successfully!'
+    reset_session
+    redirect_to root_path, notice: 'Logged out successfully!'
+  end
+
+  private
+
+  def api_call(url, payload)
+    headers = {
+      'apikey': ENV['supabase_api_key'],
+      'Content-Type': 'application/json'
+    }
+
+    response = RestClient.post("#{supabase_url}#{url}", payload.to_json, headers)
+    data = JSON.parse(response.body)
+    data
   end
 end
